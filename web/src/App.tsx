3501d75db7754635d804fe202b292831c65dab02
MarcosVini9999 <marcosviniciusandradedesousa@hotmail.com>
2022-09-23 15:00:35 -0300

    chore: adição background, gradientes, título e lista na tela

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,66 @@
 import React from "react";
 import "./styles/main.css";
-
-interface TituloProps {
-  title: string;
-}
-
-const Titulo: React.FC<TituloProps> = ({ title }) => {
-  return <h1>{title}</h1>;
-};
+import logoSvg from "./assets/Logo.svg";
 
 const App: React.FC = () => {
   return (
-    <div className="bg-violet-500">
-      <Titulo title="Hello World"></Titulo>
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoSvg} />
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{" "}
+        <span className="bg-your-duo-gradient text-transparent bg-clip-text">
+          duo
+        </span>{" "}
+        está aqui.
+      </h1>
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/image-1.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-background-title-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="text-sm text-zinc-300 block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative">
+          <img src="/image-2.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-background-title-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Dota 2</strong>
+            <span className="text-sm text-zinc-300 block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative">
+          <img src="/image-3.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-background-title-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">CS-GO</strong>
+            <span className="text-sm text-zinc-300 block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative">
+          <img src="/image-4.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-background-title-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              World Of Warcraft
+            </strong>
+            <span className="text-sm text-zinc-300 block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative">
+          <img src="/image-5.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-background-title-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Apex</strong>
+            <span className="text-sm text-zinc-300 block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative">
+          <img src="/image-6.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-background-title-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Fortnite</strong>
+            <span className="text-sm text-zinc-300 block">4 anúncios</span>
+          </div>
+        </a>
+      </div>
     </div>
   );
 };
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.tsx"],
+  content: ["./src/**/*.tsx", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        backgroundGalaxy: 'url("/Fundo.svg")',
+        "your-duo-gradient":
+          "linear-gradient(89.86deg, #9572FC 32.08%, #43E7AD 33.94%, #E1D55D 84.57%)",
+        "background-title-game-gradient":
+          "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)",
+      },
+    },
   },
   plugins: [],
 };
