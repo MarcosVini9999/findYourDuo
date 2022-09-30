3e51298ef5bea9ee781ca92bb62044a5c91933cd
MarcosVini9999 <marcosviniciusandradedesousa@hotmail.com>
2022-09-30 16:51:15 -0300

    feat: adição das portas dos métodos http e instalação do prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,13 +2,29 @@ import express from "express";
 
 const app = express();
 
-app.get("/ads", (request, response) => {
+app.get("/games", (request, response) => {
+  return response.json([]);
+});
+
+app.post("/ads", (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get("/games/:id/ads", (request, response) => {
+  const gameId = request.params.id;
+
   return response.json([
     { id: 1, nome: "user" },
     { id: 2, nome: "user2" },
     { id: 3, nome: "user3" },
     { id: 4, nome: "user4" },
   ]);
+});
+
+app.get("/ads/:id/discord", (request, response) => {
+  const adId = request.params.id;
+
+  return response.json([]);
 });
 
 app.listen(3333);
